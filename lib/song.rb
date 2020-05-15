44c0d467d50b2a1ee99260d970cc9f8bd3bc0cbf
DAZUN XING <dazun.xing@gmail.com>
2020-05-15 14:05:17 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,58 @@
+class Song
+  @@count =0
+  @@artists =[]
+  @@genres =[]
+  
+  attr_accessor :name, :artist, :genre
+  def initialize (name,artist,genre)
+    @name =name
+    @artist =artist
+    @genre =genre
+    @@count +=1
+    # if !(@@genres.include?(genre))
+    #   @@genres << genre
+    # end
+      
+    # if !(@@artists.include?(artist))
+    #   @@artists << artist
+    # end  
+      
+    @@genres << genre
+    @@artists << artist
+  end
+  
+  def self.count
+    @@count
+  end
+  
+  def self.genres
+    genres=[]
+    if !(genres.include?(@genre))
+      genres << @genre
+    end
+  end
+  
+  def self.artists
+     artists=[]
+    if !(artists.include?(@artist))
+      artists << @artist
+    end
+  end
+  
+  def self.genre_count
+    hash=Hash.new(0)
+    @@genres.each do |x|
+      hash[x] +=1
+    end
+    hash
+  end
+  
+   def self.artist_count
+    hash=Hash.new(0)
+    @@artists.each do |x|
+      hash[x] +=1
+    end
+    hash
+  end
+  
+end
